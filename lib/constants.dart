ef0e17014dd6213d7b5c6a06f6ffc66ef60c5c4b
Mohamed Nguira <m4moupro@gmail.com>
2022-08-26 18:44:42 +0100

    first

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Constants{
+  static Color blue = Colors.black12;
+}
---
+++ lib/screens/signin.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+  @override
+  State<SignInPage> createState() => SignInPageState();
+}
+
+class SignInPageState extends State<SignInPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SingleChildScrollView(child:
+        Column()
+      );
+  }
+}
