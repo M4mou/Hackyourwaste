30c6d4cc48e07fc674a333632492d05ffdd229e0
ahmed <ahmedtrabelsi.8b7@gmail.com>
2022-08-27 06:29:26 +0100

    test

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hazebi/screens/QR_code_scanner_screen.dart';
 import 'package:hazebi/screens/homesubscreen.dart';
 import 'package:hazebi/screens/landing_screen.dart';
 import 'package:hazebi/screens/profile_screen.dart';
